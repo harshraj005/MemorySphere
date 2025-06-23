596f02bad3201f293a7078f05e4e488027c5e524
Harsh Raj <132002718+harshraj005@users.noreply.github.com>
2025-06-23 16:01:22 +0530

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,6 @@ import { Tabs } from 'expo-router';
 import { useTheme } from '@/contexts/ThemeContext';
 import { Home, MessageCircle, SquareCheck as CheckSquare, User, Crown } from 'lucide-react-native';
 
-
 export default function TabsLayout() {
   const { colors } = useTheme();
 
